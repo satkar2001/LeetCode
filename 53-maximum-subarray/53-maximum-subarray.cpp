4d6fb63269ba3edec26587a0dff4f87dadc56a19
Satkar Acharya <81009537+satkar2001@users.noreply.github.com>
2022-10-10 12:21:40 +0530

    Time: 189 ms (56.91%), Space: 67.8 MB (11.02%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -6,16 +6,10 @@ public:
         int overbest=nums[0];
         
         for(int i=1;i<nums.size();i++){
-            if(currbest>=0){
-                currbest+=nums[i];
-            }
-            else{
-                currbest=nums[i];
-                
-            }
-            if(currbest>overbest){
-                overbest=currbest;
-            }
+            
+            currbest=max(nums[i],currbest+nums[i]);
+            
+            overbest=max(overbest,currbest);
             
         }
         return overbest;
